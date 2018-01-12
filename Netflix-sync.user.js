8e6c5c1fa76a8ab4e669b9524bc25d54a00c222d
Kevin Traa <ketra90@gmail.com>
2018-01-12 13:06:14 +0100

    Update Netflix-sync.user.js
    Version Bump

--- Netflix-sync.user.js
+++ Netflix-sync.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         netflix-sync
 // @namespace    https://github.com/ketra/netflix-sync
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       Ketra
 // @match        https://www.netflix.com/viewingactivity*
